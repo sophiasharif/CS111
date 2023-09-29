f31e926d077037bc98420a40ed8ccf4f4fb3b25a
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-09-29 14:36:16 -0700

    newline

--- lab0/proc_count.c
+++ lab0/proc_count.c
@@ -13,7 +13,7 @@ static int proc_count(struct seq_file *m, void *v){
     for_each_process(p)
         count++;
     
-    seq_printf(m, "%d", count);
+    seq_printf(m, "%d\n", count);
     
     
 	return 0;
