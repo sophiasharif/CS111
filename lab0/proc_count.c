6b2c1879f7b2115094d96d8100fe9bcdb290c28b
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-09-29 14:34:41 -0700

    pointer declaration

--- lab0/proc_count.c
+++ lab0/proc_count.c
@@ -9,6 +9,7 @@ static struct proc_dir_entry *entry;
 static int proc_count(struct seq_file *m, void *v){
     
     int count = 0;
+    struct task_struct* p;
     for_each_process(p)
         count++;
     
