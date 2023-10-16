a32c03d6b7780f3a72c35243ffbbac980a0f0126
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-10-16 13:02:34 -0700

    refactoring

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -4,95 +4,74 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+void errorExit(const char *message)
+{
+	perror(message);
+	exit(1);
+}
+
+// creates a pipe, executes command in a child process, returns fd for read end of pipe.
+int executeFirstCommand(char *command)
+{
+	int fds[2]; // fd[0] -> read end, fd[1] -> write end
+	if (pipe(fds) == -1)
+		errorExit("pipe failed");
+	pid_t p = fork();
+
+	if (p < 0)
+		errorExit("fork failed");
+	else if (p == 0) // child executes first command
+	{
+		dup2(fds[1], 1); // point stdout (fd 1) to write end of pipe (fd[1])
+		close(fds[1]);	 // close write end of pipe for EOF
+		execlp(command, command, NULL);
+		errorExit("execlp failed");
+	}
+	else
+	{
+		int cpid = wait(NULL); // wait for child to finish command
+		close(fds[1]);		   // close write end of pipe for EOF; now all write fds are closed.
+		return fds[0];		   // return read end of pipe
+	}
+	return -1;
+}
+
+void executeLastCommand(char *command, int readEndOfPipe)
+{
+	pid_t p2 = fork(); // create new child
+
+	if (p2 < 0)
+		errorExit("fork failed");
+	else if (p2 == 0)
+	{
+		dup2(readEndOfPipe, 0); // point stdin to read end of pipe
+		close(readEndOfPipe);
+		execlp(command, command, NULL);
+		errorExit("execlp failed");
+	}
+	else
+	{
+		close(readEndOfPipe);
+		int cpid2 = wait(NULL); // wait for process to finish
+	}
+}
+
 int main(int argc, char *argv[])
 {
+	if (argc < 2)
+	{
+		fprintf(stderr, "Provide at least one command.\n");
+		exit(1);
+	}
+
 	// only one process
 	if (argc == 2)
-	{
 		execlp(argv[1], argv[1], NULL);
-	}
 
 	// two processes
 	if (argc == 3)
 	{
-		int fds[2]; // fd[0] -> read end, fd[1] -> write end
-		pipe(fds);
-		pid_t p = fork();
-
-		if (p < 0)
-		{
-			perror("fork fail");
-			exit(1);
-		}
-		else if (p == 0)
-		{
-			// point stdout (fd 1) to write end of pipe (fd[1])
-			printf("hello from first child!\n");
-			dup2(fds[1], 1);
-			close(fds[1]);
-
-			// execute first command
-			execlp(argv[1], argv[1], NULL);
-		}
-		else
-		{
-			int cpid = wait(NULL);
-			printf("child %d exited\n", cpid);
-
-			// create new child
-			pid_t p2 = fork();
-
-			if (p2 < 0)
-			{
-				perror("fork failed");
-				exit(1);
-			}
-			else if (p2 == 0)
-			{
-
-				// point stdin to read end of pipe
-				close(fds[1]); // close write end of pipe
-				dup2(fds[0], 0);
-				close(fds[0]);
-				execlp(argv[2], argv[2], NULL);
-			}
-			else
-			{
-				close(fds[1]); // close write end of pipe
-				int cpid2 = wait(NULL);
-				printf("child %d exited\n", cpid2);
-			}
-		}
-	}
-
-	// three arguments
-	else
-	{
-		// printf("hello from parent!\n");
-		// int fds2[2];
-		// pipe(fds2);
-		// pid_t p2 = fork();
-		// if (p2 < 0)
-		// {
-		// 	perror("fork fail");
-		// 	exit(1);
-		// }
-		// if (p2 == 0)
-		// {
-		// 	printf("hello from second child!\n");
-		// 	// point stdin to read end of pipe
-		// 	dup2(fds[0], 0);
-		// 	close(fds[0]);
-		// 	// point stdout to write end of second pipe
-		// 	dup2(fds2[1], 1);
-		// 	close(fds2[1]);
-		// 	// execute second command
-		// 	execlp(argv[2], argv[2], NULL);
-		// }
-		// else
-		// {
-		// 	printf("hello from parent again!\n");
-		// 	dup2(fds2[0], 0);
-		// }
+		int readEndOfFirstPipe = executeFirstCommand(argv[1]);
+		executeLastCommand(argv[2], readEndOfFirstPipe);
 	}
 }
