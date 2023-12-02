7cc06f180cc10a33f395ff5f8b64dc5b68ccfa6c
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-12-01 21:02:35 -0800

    inodes done

--- lab4/ext2-create.c
+++ lab4/ext2-create.c
@@ -405,9 +405,24 @@ void write_inode_table(int fd)
 	hello_world_inode.i_dtime = 0;
 	hello_world_inode.i_gid = 1000;
 	hello_world_inode.i_links_count = 1;
-	hello_world_inode.i_blocks = 1; /* These are oddly 512 blocks */
+	hello_world_inode.i_blocks = 2; /* These are oddly 512 blocks */
 	hello_world_inode.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
 	write_inode(fd, HELLO_INO, &hello_world_inode);
+
+	// hello world inode
+	struct ext2_inode hello_inode = {0};
+	char *path_to_hello_world = "./hello-world";
+	hello_inode.i_mode = EXT2_S_IFLNK | EXT2_S_IRUSR | EXT2_S_IWUSR | EXT2_S_IRGRP | EXT2_S_IROTH;
+	hello_inode.i_uid = 1000;
+	hello_inode.i_size = strlen(path_to_hello_world);
+	hello_inode.i_atime = current_time;
+	hello_inode.i_ctime = current_time;
+	hello_inode.i_mtime = current_time;
+	hello_inode.i_dtime = 0;
+	hello_inode.i_gid = 1000;
+	hello_inode.i_links_count = 1;
+	hello_inode.i_blocks = 0; /* These are oddly 512 blocks */
+	memcpy(hello_inode.i_block, path_to_hello_world, hello_inode.i_size);
 }
 
 void write_root_dir_block(int fd)
