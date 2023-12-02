cddb7cede373165a8a3532ec093fff8661723a84
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-12-01 18:15:58 -0800

    update

--- lab4/ext2-create.c
+++ lab4/ext2-create.c
@@ -334,7 +334,7 @@ void write_inode_bitmap(int fd)
 	u8 map_value[BLOCK_SIZE];
 	memset(map_value, 0xFF, BLOCK_SIZE);
 	map_value[1] = 0b00011111;
-	// memset(map_value + 2, 0, NUM_INODES / 8 - 2);
+	memset(map_value + 2, 0, NUM_INODES / 8 - 2);
 	// map_value[BLOCK_SIZE / 8 - 1] = 0b10000000;
 
 	if (write(fd, map_value, BLOCK_SIZE) != BLOCK_SIZE)
