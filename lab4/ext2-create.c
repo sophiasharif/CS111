3136c894ce7c66957b8c697312ec662634836d27
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-12-01 21:16:24 -0800

    fix??

--- lab4/ext2-create.c
+++ lab4/ext2-create.c
@@ -449,7 +449,7 @@ void write_root_dir_block(int fd)
 	bytes_remaining -= parent_entry.rec_len;
 
 	struct ext2_dir_entry lost_and_found_entry = {0};
-	dir_entry_set(lost_and_found_entry, LOST_AND_FOUND_INO, "./lost+found");
+	dir_entry_set(lost_and_found_entry, LOST_AND_FOUND_INO, "./lost\+found");
 	dir_entry_write(lost_and_found_entry, fd);
 
 	bytes_remaining -= lost_and_found_entry.rec_len;
