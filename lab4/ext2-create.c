0f03a8f159704e5a416c8d84afdb5ea363a89bfe
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-12-01 22:20:42 -0800

    update

--- lab4/ext2-create.c
+++ lab4/ext2-create.c
@@ -512,10 +512,11 @@ void write_hello_world_file_block(int fd)
 	{
 		errno_exit("write");
 	}
+	memset(off + 16, 0x00, BLOCK_SIZE - 16);
 
-	struct ext2_dir_entry fill_entry = {0};
-	fill_entry.rec_len = BLOCK_SIZE;
-	dir_entry_write(fill_entry, fd);
+	// struct ext2_dir_entry fill_entry = {0};
+	// fill_entry.rec_len = BLOCK_SIZE-12;
+	// dir_entry_write(fill_entry, fd);
 }
 
 int main(int argc, char *argv[])
